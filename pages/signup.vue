bd473e7904603471d316924897e68238f52e6b98
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-05 13:43:58 +0530

    initial commit

---
+++ pages/signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-pink-600 text-white mt-16">
+    <h1 class="text-center text-4xl">
+      Sign-Up
+    </h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
