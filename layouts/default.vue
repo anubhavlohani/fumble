952f848f3fbdec793fe99251f283b2fd1eb621b7
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-06 19:45:37 +0530

    changed theme to rose from pink

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="h-screen bg-pink-100">
+  <div class="h-screen bg-rose-100">
     <header>
-      <ul class="flex flex-row space-x-5 items-center px-5 h-16 w-screen text-xl text-white bg-pink-500">
+      <ul class="flex flex-row space-x-5 items-center px-5 h-16 w-screen text-xl text-white bg-rose-500">
         <li class="text-3xl"><NuxtLink to="/">Fumble</NuxtLink></li>
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/signup">Sign-Up</NuxtLink></li>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto w-full max-w-xs rounded-lg bg-pink-500 text-white mt-16 py-4">
+  <div class="mx-auto w-full max-w-xs rounded-lg bg-rose-500 text-white mt-16 py-4">
     <div class="text-center">
       <h1 class="my-6 text-4xl">
         Sign-Up
@@ -8,14 +8,14 @@
     <div class="my-4">
       <form class="text-xl px-4">
         <label class="block text-left mb-2" for="name">Name:</label>
-        <input v-model="name" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-pink-100" required>
+        <input v-model="name" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-rose-100" required>
         <label class="block text-left mb-2" for="username">Username:</label>
-        <input v-model="username" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-pink-100" required>
+        <input v-model="username" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-rose-100" required>
         <label class="block text-left mb-2" for="email">Email:</label>
-        <input v-model="email" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-pink-100" required>
+        <input v-model="email" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-rose-100" required>
         <label class="block text-left mb-2" for="password">Password:</label>
-        <input v-model="password" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-pink-100" required>
-        <button type="submit" class="text-center text-lg py-1 px-2 rounded bg-pink-600 hover:bg-pink-700 active:bg-pink-800">Sign Up</button>
+        <input v-model="password" class="shadow appearance-none border rounded w-full mb-4 py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-rose-100" required>
+        <button type="submit" class="text-center text-lg py-1 px-2 rounded bg-rose-600 hover:bg-rose-700 active:bg-rose-800">Sign Up</button>
       </form>
     </div>
   </div>
