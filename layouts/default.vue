571f0f74fa505957183e6d7fc93b20c11e7fe851
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-12 20:29:20 +0530

    re-added fumble logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <header>
       <ul class="px-5 h-16 w-screen flex flex-row space-x-5 items-center text-xl text-white shadow-xl bg-blue-600">
-        <li class="text-3xl"><NuxtLink to="/">XYZ</NuxtLink></li>
+        <li class="text-3xl"><NuxtLink to="/">Fumble</NuxtLink></li>
         <li class=""><NuxtLink to="/">Home</NuxtLink></li>
         <li class=""><NuxtLink to="/signup">Signup</NuxtLink></li>
         <li class=""><NuxtLink to="/login">Login</NuxtLink></li>
