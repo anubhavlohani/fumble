52e940b759f26c2d9e3d3eef2db619650fab8037
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-14 20:32:17 +0530

    added album name and feature artist details

--- components/Story.vue
+++ components/Story.vue
@@ -1,25 +1,46 @@
 <template>
   <div class="my-12 mx-auto flex flex-col text-center w-full max-w-md rounded-lg bg-zinc-100 text-white">
-    <!-- Album cover -->
+    <!-- Album details -->
     <div class="py-4 px-4 mx-auto">
-      <img :src="story.track.album.images[1]" :alt="`${story.track.album.name} cover art`" class="rounded-lg">
+      <div class="gap-y-4 flex flex-col">
+        <img :src="story.track.album.images[1]" :alt="`${story.track.album.name} cover art`" class="rounded-lg">
+        <div class="text-lg text-black">
+          {{ story.track.album.name }}
+        </div>
+      </div>
     </div>
+    
     <!-- Song details -->
-    <div class="py-4 px-4 rounded-t-lg bg-black text-lg">
-      <div>
+    <div class="py-2 px-2 rounded-t-lg bg-black">
+      <div class="text-lg">
         {{ story.track.name }}
       </div>
-      <div class="gap-x-4 flex flex-row justify-center items-center">
-        <div>
-          <img :src="story.track.artists[0].images[2]" :alt="`${story.track.artists[0].name} picture`" class="w-8 block rounded-full">
+      <div class="py-2 gap-x-2 flex flex-row justify-center items-center">
+        By:
+        <div class="gap-x-1 flex flex-row justify-center items-center">
+          <div>
+            <img :src="story.track.artists[0].images[2]" :alt="`${story.track.artists[0].name} picture`" class="w-8 block rounded-full">
+          </div>
+          <div>
+            {{ story.track.artists[0].name }}
+          </div>
         </div>
-        <div>
-          {{ story.track.artists[0].name }}
+      </div>
+      <div v-if="story.track.artists.length > 1" class="gap-x-2 flex flex-row flex-wrap justify-center items-center">
+        Feat:
+        <div v-for="feature in story.track.artists.slice(1,)" :key="feature" class="gap-x-1 flex flex-row justify-center items-center">
+          <div>
+            <img :src="feature.images[2]" :alt="`${feature.name} picture`" class="w-8 block rounded-full">
+          </div>
+          <div>
+            {{ feature.name }}
+          </div>
         </div>
       </div>
     </div>
+    
     <!-- Story deatils -->
-    <div class="py-4 px-4 text-left rounded-b-lg bg-black">
+    <div class="py-2 px-4 text-left rounded-b-lg bg-black">
       <div class="font-semibold">
         {{ story.username }}
       </div>
