0b6785b69bea330dbcb8d7bfc49c4cec774860a3
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-20 19:14:07 +0530

    Removed access token as data val

--- store/user.js
+++ store/user.js
@@ -2,7 +2,6 @@ import { defineStore } from "pinia"
 
 export const useUserStore = defineStore('user', () => {
   const user = ref(null)
-  const access_token = ref(null)
 
-  return { user, access_token }
+  return { user }
 })
