4912aa7a37fe092c07a05343ad1ffbbe69e6a306
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-20 21:02:58 +0530

    can now unlike story

--- components/Story.vue
+++ components/Story.vue
@@ -46,7 +46,7 @@
     <!-- Story deatils -->
     <div class="py-2 px-4 text-left rounded-b-lg bg-black">
       <div class="h-6 w-6 hover:opacity-70">
-        <svg v-if="currentUser.user && story.liked_by.includes(currentUser.user.id)" class="fill-red-500" viewBox="0 0 48 48">
+        <svg v-if="currentUser.user && story.liked_by.includes(currentUser.user.id)" @click="unlikeStory()" class="fill-red-500" viewBox="0 0 48 48">
           <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
         </svg>
 
@@ -85,13 +85,42 @@ function likeStory () {
     alert('Session expired, please login again.')
     return navigateTo('/login')
   }
-  
+
   const likeData = {
     user_id: currentUser.user.id,
     story_id: story.id
   }
   fetch(`${apiURL}/like-story`, {
     method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
+      'Content-Type': 'application/json',
+      "ngrok-skip-browser-warning": true
+    },
+    body: JSON.stringify(likeData)
+  }).then(res => processServerResponse(res)).catch(error => alert(error))
+}
+
+function unlikeStory () {
+  story.liked_by = story.liked_by.filter(user_id => user_id !== currentUser.user.id)
+  
+  let processServerResponse = async (res) => {
+    if (!res.ok) {
+      return navigateTo('/login')
+    }
+  }
+
+  if (localStorage.getItem('access_token') === null) {
+    alert('Session expired, please login again.')
+    return navigateTo('/login')
+  }
+  
+  const likeData = {
+    user_id: currentUser.user.id,
+    story_id: story.id
+  }
+  fetch(`${apiURL}/delete-like`, {
+    method: 'DELETE',
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
       'Content-Type': 'application/json',
