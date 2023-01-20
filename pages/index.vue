7f7e6d4cc5bc3a262e02e2ea879ce27c912c1f61
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-20 21:58:07 +0530

    load data after mount

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div v-if="pending">
-      <Spinner />
-    </div>
-    <div v-else>
-      <div v-for="story in data.stories" :key="story">
+    <div v-if="stories.length > 0">
+      <div v-for="story in stories" :key="story">
         <Story :story="story" />
       </div>
+    </div>
+    <div v-else>
+      <Spinner />
     </div>
   </div>
 </template>
@@ -16,6 +16,7 @@ import { useUserStore } from '~~/store/user';
 
 const { apiURL } = useRuntimeConfig()
 const currentUser = useUserStore()
+const stories = ref([])
 
 onBeforeMount(() => {
   // verify access token
@@ -41,11 +42,28 @@ onBeforeMount(() => {
   }).then(res => processServerResponse(res)).catch(error => alert(error))
 })
 
-const { pending, data } = useAsyncData('stories', () => $fetch(`${apiURL}/all-stories`, {
-  headers: {
-    "ngrok-skip-browser-warning": true
+onMounted(() => {
+  let processServerResponse = async (res) => {
+    if (res.ok) {
+      const resData = await res.json()
+      stories.value = resData.stories
+    } else {
+      return navigateTo('/login')
+    }
   }
-}))
+
+  if (localStorage.getItem('access_token') === null) {
+    alert('Session expired, please login again.')
+    return navigateTo('/login')
+  }
+  fetch(`${apiURL}/all-stories`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
+      "ngrok-skip-browser-warning": true
+    }
+  }).then(res => processServerResponse(res)).catch(error => alert(error))
+})
 </script>
 
 <style scoped>
