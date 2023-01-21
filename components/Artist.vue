d716efc463cfdaab71ecea941599efe8915ea6ad
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-21 19:34:20 +0530

    bigger if enlarged

--- components/Artist.vue
+++ components/Artist.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex flex-col items-center justify-center gap-2">
+  <div class="flex flex-col items-center justify-center" :class="enlarged ? 'gap-2' : 'gap-1'">
     <a :href="artist.spotify_url" target="_blank" rel="noopener noreferrer" class="hover:opacity-80">
-      <img :src="artist.images[2]" :alt="`${artist.name} picture`" class="w-6 md:w-20 block rounded-full">
+      <img :src="artist.images[2]" :alt="`${artist.name} picture`" class="w-12 md:w-20 block rounded-full" :class="enlarged ? 'w-12 md:20' : 'w-8 md:w-12'">
     </a>
-    <a :href="artist.spotify_url" target="_blank" rel="noopener noreferrer" class="hover:bg-white hover:text-black">
+    <a :href="artist.spotify_url" target="_blank" rel="noopener noreferrer" class="hover:bg-white hover:text-black" :class="enlarged ? '' : 'text-sm'">
       {{ artist.name }}
     </a>
   </div>
 </template>
 
 <script setup>
-const { artist } = defineProps(['artist'])
+const { artist, enlarged } = defineProps(['artist', 'enlarged'])
 </script>
