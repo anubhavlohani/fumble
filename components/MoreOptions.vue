f18e526d5557304b2de80ab84e99146af4dc745d
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-27 18:58:52 +0530

    added more options buttons to stories

---
+++ components/MoreOptions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button>
+      <svg class="h-8 w-8 fill-black" viewBox="0 0 24 24">
+        <circle cx="6" cy="12" r="1.5"></circle>
+        <circle cx="12" cy="12" r="1.5"></circle>
+        <circle cx="18" cy="12" r="1.5"></circle>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
