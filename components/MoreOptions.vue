a5562998a15ee33181f3757b31a84a7b27af5855
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-28 14:19:18 +0530

    resized circles

--- components/MoreOptions.vue
+++ components/MoreOptions.vue
@@ -1,9 +1,9 @@
 <template>
   <button @click="showOptions=true">
-    <svg class="h-8 w-8 fill-black" viewBox="0 0 24 24">
-      <circle cx="6" cy="12" r="1.5"></circle>
-      <circle cx="12" cy="12" r="1.5"></circle>
-      <circle cx="18" cy="12" r="1.5"></circle>
+    <svg class="h-4 w-4 fill-black" viewBox="0 0 24 24">
+      <circle cx="3" cy="12" r="2.5"></circle>
+      <circle cx="12" cy="12" r="2.5"></circle>
+      <circle cx="21" cy="12" r="2.5"></circle>
     </svg>
   </button>
 
